67b55a73b1776e83d97d57eb4bda3415701569ea
Daniel Rauschenberger <rauschenberger_daniel@web.de>
2019-05-25 21:43:10 +0200

    stacking chunks

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Chunk
+{
+    private readonly int size;
+    private readonly int resolution;
+    private readonly Vector3 offset;
+    private readonly Func<float, float, float, float> noiseFunc;
+    private readonly float noiseSize;
+
+    private readonly float[] values;
+    private readonly Vector3[] nodes;
+
+    public Chunk(int size, int resolution, Vector3 offset, Func<float, float, float, float> noiseFunc, float noiseSize)
+    {
+        this.size = size + 1;
+        this.resolution = resolution + 1;
+        this.offset = offset;
+        this.noiseFunc = noiseFunc;
+        this.noiseSize = noiseSize;
+
+        values = new float[Mathf.RoundToInt(Mathf.Pow(this.resolution, 3))];
+        nodes = new Vector3[Mathf.RoundToInt(Mathf.Pow(this.resolution, 3))];
+    }
+
+    public void CalculatePoints()
+    {
+        for (int x = 0; x < resolution; x++)
+        {
+            for (int y = 0; y < resolution; y++)
+            {
+                for (int z = 0; z < resolution; z++)
+                {
+                    int index = GetIndex(x, y, z);
+                    var pos = offset * size + new Vector3(x, y, z) * size / resolution;
+                    var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    go.transform.position = pos;
+                    go.transform.localScale = Vector3.one / 10;
+                    nodes[index] = pos;
+                    values[index] = noiseFunc(offset.x + (float) x / resolution / noiseSize,
+                        offset.y + (float) y / resolution / noiseSize,
+                        offset.z + (float) z / resolution / noiseSize);
+                }
+            }
+        }
+    }
+
+    public Vector3[] March(float isoLevel)
+    {
+        var vertices = new List<Vector3>();
+
+        for (int x = 0; x < resolution - 1; x++)
+        {
+            for (int y = 0; y < resolution - 1; y++)
+            {
+                for (int z = 0; z < resolution - 1; z++)
+                {
+                    var indices = new[]
+                    {
+                        GetIndex(x + 0, y + 0, z + 0),
+                        GetIndex(x + 0, y + 1, z + 0),
+                        GetIndex(x + 1, y + 1, z + 0),
+                        GetIndex(x + 1, y + 0, z + 0),
+                        GetIndex(x + 0, y + 0, z + 1),
+                        GetIndex(x + 0, y + 1, z + 1),
+                        GetIndex(x + 1, y + 1, z + 1),
+                        GetIndex(x + 1, y + 0, z + 1)
+                    };
+
+                    var corners = new Vector3[8];
+                    int cubeIndex = 0;
+                    for (int i = 0; i < 8; i++)
+                    {
+                        corners[i] = nodes[indices[i]];
+                        if (values[indices[i]] > isoLevel)
+                            cubeIndex |= Mathf.RoundToInt(Mathf.Pow(2, i));
+                    }
+
+                    int edgeIndex = Extensions.EdgeTable[cubeIndex];
+
+                    var vertList = new Vector3[12];
+                    for (int i = 0; i < 12; i++)
+                    {
+                        if ((edgeIndex & Mathf.RoundToInt(Mathf.Pow(2, i))) != 0)
+                        {
+                            vertList[i] = InterpolateVerts(isoLevel, corners[i % 8], corners[Extensions.ValueTable[i]],
+                                values[indices[i % 8]], values[indices[Extensions.ValueTable[i]]]);
+                        }
+                    }
+
+                    for (int i = 0; Extensions.TriTable[cubeIndex][i] != -1; i += 3)
+                    {
+                        vertices.Add(vertList[Extensions.TriTable[cubeIndex][i]]);
+                        vertices.Add(vertList[Extensions.TriTable[cubeIndex][i + 1]]);
+                        vertices.Add(vertList[Extensions.TriTable[cubeIndex][i + 2]]);
+                    }
+                }
+            }
+        }
+
+        return vertices.ToArray();
+    }
+
+    private Vector3 InterpolateVerts(float isoLevel, Vector3 p1, Vector3 p2, float v1, float v2)
+    {
+        float t = (isoLevel - v1) / (v2 - v1);
+        return p1 + t * (p2 - p1);
+    }
+
+    private int GetIndex(int x, int y, int z) => resolution * resolution * z + resolution * y + x;
+}
--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -5,135 +5,151 @@ using UnityEngine;
 
 public class WorldController : MonoBehaviour
 {
-    [SerializeField] private int size;
-    [SerializeField] private int resolution;
-    [SerializeField, Range(0, 5)] private int offset;
-    [SerializeField, Range(0, 1)] private float noiseSize;
-    [SerializeField, Range(0, 1)] private float isoValue;
-
-    private float[] values;
-    private Vector3[] nodes;
+//    [SerializeField] private int size;
+//    [SerializeField] private int resolution;
+//    [SerializeField, Range(0, 5)] private int offset;
+//    [SerializeField, Range(0, 1)] private float noiseSize;
+//    [SerializeField, Range(0, 1)] private float isoValue;
+//
+//    private float[] values;
+//    private Vector3[] nodes;
     private MeshFilter _filter;
 
-    private void Awake()
-    {
-        size++;
-        resolution++;
-
-        var noise = new FastNoise();
-
-        values = new float[Mathf.RoundToInt(Mathf.Pow(resolution, 3))];
-        nodes = new Vector3[Mathf.RoundToInt(Mathf.Pow(resolution, 3))];
-
-        for (int x = 0; x < resolution; x++)
-        {
-            for (int y = 0; y < resolution; y++)
-            {
-                for (int z = 0; z < resolution; z++)
-                {
-                    var pos = new Vector3(x, y, z) * size / resolution;
-//                    var obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-//                    obj.transform.position = pos;
-                    nodes[x + resolution * y + resolution * resolution * z] = pos;
-                }
-            }
-        }
-
-        Generate();
-    }
-
+//
+//    private void Awake()
+//    {
+//        size++;
+//        resolution++;
+//
+//        var noise = new FastNoise();
+//
+//        values = new float[Mathf.RoundToInt(Mathf.Pow(resolution, 3))];
+//        nodes = new Vector3[Mathf.RoundToInt(Mathf.Pow(resolution, 3))];
+//
+//        for (int x = 0; x < resolution; x++)
+//        {
+//            for (int y = 0; y < resolution; y++)
+//            {
+//                for (int z = 0; z < resolution; z++)
+//                {
+//                    var pos = new Vector3(x, y, z) * size / resolution;
+////                    var obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+////                    obj.transform.position = pos;
+//                    nodes[x + resolution * y + resolution * resolution * z] = pos;
+//                }
+//            }
+//        }
+//
+//        Generate();
+//    }
+//
     private float Noise(float x, float y, float z)
     {
         return (Mathf.PerlinNoise(x, y) + Mathf.PerlinNoise(y, x) +
                 Mathf.PerlinNoise(x, z) + Mathf.PerlinNoise(z, x) +
                 Mathf.PerlinNoise(y, z) + Mathf.PerlinNoise(z, y)) / 6;
     }
+//
+//    private void Update()
+//    {
+//        Generate();
+//    }
+//
+//    private void Generate()
+//    {
+//        var triangles = new List<Triangle>();
+//
+//        for (int x = 0; x < resolution - 1; x++)
+//        {
+//            for (int y = 0; y < resolution - 1; y++)
+//            {
+//                for (int z = 0; z < resolution - 1; z++)
+//                {
+//                    var indices = new int[]
+//                    {
+//                        GetIndex(x + 0, y + 0, z + 0),
+//                        GetIndex(x + 0, y + 1, z + 0),
+//                        GetIndex(x + 1, y + 1, z + 0),
+//                        GetIndex(x + 1, y + 0, z + 0),
+//                        GetIndex(x + 0, y + 0, z + 1),
+//                        GetIndex(x + 0, y + 1, z + 1),
+//                        GetIndex(x + 1, y + 1, z + 1),
+//                        GetIndex(x + 1, y + 0, z + 1)
+//                    };
+//
+//                    int index = GetIndex(x, y, z);
+//                    values[index] = Noise(offset + (float) x / resolution / noiseSize,
+//                        offset + (float) y / resolution / noiseSize,
+//                        offset + (float) z / resolution / noiseSize);
+//
+//                    cubePosition = Vector3.Lerp(nodes[indices[0]], nodes[indices[6]], 0.5f);
+//
+//                    var corners = new Vector3[8];
+//                    int cubeIndex = 0;
+//                    for (int i = 0; i < 8; i++)
+//                    {
+//                        corners[i] = nodes[indices[i]];
+//                        if (values[indices[i]] > isoValue)
+//                            cubeIndex |= Mathf.RoundToInt(Mathf.Pow(2, i));
+//                    }
+//
+//                    int edgeIndex = Extensions.EdgeTable[cubeIndex];
+//
+//                    var vertList = new Vector3[12];
+//                    for (int i = 0; i < 12; i++)
+//                    {
+//                        if ((edgeIndex & Mathf.RoundToInt(Mathf.Pow(2, i))) != 0)
+//                        {
+//                            vertList[i] = InterpolateVerts(corners[i % 8], corners[Extensions.ValueTable[i]],
+//                                values[indices[i % 8]], values[indices[Extensions.ValueTable[i]]]);
+//                        }
+//                    }
+//
+//                    int current = 0;
+//
+//                    for (int i = 0; Extensions.TriTable[cubeIndex][i] != -1; i += 3)
+//                    {
+//                        var p0 = vertList[Extensions.TriTable[cubeIndex][i]];
+//                        var p1 = vertList[Extensions.TriTable[cubeIndex][i + 1]];
+//                        var p2 = vertList[Extensions.TriTable[cubeIndex][i + 2]];
+//                        triangles.Add(new Triangle(new[] {p0, p1, p2}));
+//                    }
+//                }
+//            }
+//        }
+//
+//        RenderMesh(triangles.GetVertices());
+//    }
+//
+//    private Vector3 InterpolateVerts(Vector3 p1, Vector3 p2, float v1, float v2)
+//    {
+//        float t = (isoValue - v1) / (v2 - v1);
+//        return p1 + t * (p2 - p1);
+//    }
+//
+//    private Vector3[] verts;
 
-    private void Update()
+    [SerializeField] private Material material;
+
+    private void Awake()
     {
-        Generate();
-    }
-
-    private void Generate()
-    {
-        var triangles = new List<Triangle>();
-
-        for (int x = 0; x < resolution - 1; x++)
+        for (int i = 0; i < 2; i++)
         {
-            for (int y = 0; y < resolution - 1; y++)
-            {
-                for (int z = 0; z < resolution - 1; z++)
-                {
-                    var indices = new int[]
-                    {
-                        GetIndex(x + 0, y + 0, z + 0),
-                        GetIndex(x + 0, y + 1, z + 0),
-                        GetIndex(x + 1, y + 1, z + 0),
-                        GetIndex(x + 1, y + 0, z + 0),
-                        GetIndex(x + 0, y + 0, z + 1),
-                        GetIndex(x + 0, y + 1, z + 1),
-                        GetIndex(x + 1, y + 1, z + 1),
-                        GetIndex(x + 1, y + 0, z + 1)
-                    };
-
-                    int index = GetIndex(x, y, z);
-                    values[index] = Noise(offset + (float) x / resolution / noiseSize,
-                        offset + (float) y / resolution / noiseSize,
-                        offset + (float) z / resolution / noiseSize);
-
-                    cubePosition = Vector3.Lerp(nodes[indices[0]], nodes[indices[6]], 0.5f);
-
-                    var corners = new Vector3[8];
-                    int cubeIndex = 0;
-                    for (int i = 0; i < 8; i++)
-                    {
-                        corners[i] = nodes[indices[i]];
-                        if (values[indices[i]] > isoValue)
-                            cubeIndex |= Mathf.RoundToInt(Mathf.Pow(2, i));
-                    }
-
-                    int edgeIndex = Extensions.EdgeTable[cubeIndex];
-
-                    var vertList = new Vector3[12];
-                    for (int i = 0; i < 12; i++)
-                    {
-                        if ((edgeIndex & Mathf.RoundToInt(Mathf.Pow(2, i))) != 0)
-                        {
-                            vertList[i] = InterpolateVerts(corners[i % 8], corners[Extensions.ValueTable[i]],
-                                values[indices[i % 8]], values[indices[Extensions.ValueTable[i]]]);
-                        }
-                    }
-
-                    int current = 0;
-
-                    for (int i = 0; Extensions.TriTable[cubeIndex][i] != -1; i += 3)
-                    {
-                        var p0 = vertList[Extensions.TriTable[cubeIndex][i]];
-                        var p1 = vertList[Extensions.TriTable[cubeIndex][i + 1]];
-                        var p2 = vertList[Extensions.TriTable[cubeIndex][i + 2]];
-                        triangles.Add(new Triangle(new[] {p0, p1, p2}));
-                    }
-                }
-            }
+            var go = new GameObject();
+            var filter = go.AddComponent<MeshFilter>();
+            var renderer = go.AddComponent<MeshRenderer>();
+            renderer.material = material;
+            var chunk = new Chunk(20, 5, new Vector3(i, 0, 0), Noise, 1);
+            chunk.CalculatePoints();
+            RenderMesh(chunk.March(0.4f), filter);
         }
-
-        RenderMesh(triangles.GetVertices());
     }
 
-    private Vector3 InterpolateVerts(Vector3 p1, Vector3 p2, float v1, float v2)
+    private void RenderMesh(Vector3[] vertices, MeshFilter filter)
     {
-        float t = (isoValue - v1) / (v2 - v1);
-        return p1 + t * (p2 - p1);
-    }
-
-    private Vector3[] verts;
-
-    private void RenderMesh(Vector3[] vertices)
-    {
-        _filter = GetComponent<MeshFilter>();
+        Debug.Log(vertices.Length);
 
         var mesh = new Mesh();
-        verts = vertices;
         mesh.vertices = vertices;
         var triangles = new int[vertices.Length - vertices.Length % 3];
         for (int i = 0; i < triangles.Length; i += 3)
@@ -146,7 +162,7 @@ public class WorldController : MonoBehaviour
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
 
-        _filter.mesh = mesh;
+        filter.mesh = mesh;
     }
 
     private Vector3 cubePosition;
@@ -176,5 +192,5 @@ public class WorldController : MonoBehaviour
 //        }
 //    }
 
-    private int GetIndex(int x, int y, int z) => size * size * z + size * y + x;
+    //private int GetIndex(int x, int y, int z) => size * size * z + size * y + x;
 }
