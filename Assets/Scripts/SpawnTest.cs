759522b53eab0bdf3186d9f959b5835d5bf9df21
Daniel Rauschenberger <rauschenberger_daniel@web.de>
2019-08-05 14:00:03 +0200

    change folder structure

---
+++ Assets/Scripts/SpawnTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTest : MonoBehaviour
+{
+    [SerializeField] private GameObject anchor;
+    [SerializeField] private int radius;
+
+    private void Start()
+    {
+        var i = 0;
+        
+        
+        
+        var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = anchor.transform.position + new Vector3(0, -radius, 0);
+
+        go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = anchor.transform.position + new Vector3(0, -radius + 1, -1);
+        go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = anchor.transform.position + new Vector3(-1, -radius + 1, 0);
+        go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = anchor.transform.position + new Vector3(0, -radius + 1, 0);
+        go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = anchor.transform.position + new Vector3(1, -radius + 1, 0);
+        go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = anchor.transform.position + new Vector3(0, -radius + 1, 1);
+    }
+}
